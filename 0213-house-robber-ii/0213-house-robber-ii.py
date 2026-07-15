c969098aee43dae07ee7c9888cad53d2a2374dad
P MANIDEEP REDDY <patlollamanideepreddy3183@gmail.com>
2026-07-15 22:00:48 +0530

    Time: 0 ms (100%), Space: 12.4 MB (21.77%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -4,13 +4,12 @@ class Solution(object):
             return 0
         if len(nums) == 1:
             return nums[0]
-        a = 0
-        b = nums[0]
-        for num in nums[1:]:
-            r = max(a + num,b)
-            a = b
-            b = r
-        return b
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0],nums[1])
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        return dp[-1]
     def rob(self, nums):
         if not nums:
             return 0
