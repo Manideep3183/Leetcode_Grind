fa2d95832bd8b3cd9fa9373761a1bbbab60255c4
P MANIDEEP REDDY <patlollamanideepreddy3183@gmail.com>
2026-07-19 20:36:59 +0530

    Time: 169 ms (48.28%), Space: 13.8 MB (61.11%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
@@ -1,18 +1,16 @@
 class Solution(object):
     def subarraysWithKDistinct(self, nums, k):
-        def atMostK(dist_count):
+        def atMostK(k):
             hash_map = {}
             left = 0
             total = 0
             for r in range(len(nums)):
-                if hash_map.get(nums[r], 0) == 0:
-                    dist_count -= 1
                 hash_map[nums[r]] = hash_map.get(nums[r],0) + 1
-                while dist_count < 0:
+                while len(hash_map) > k:
                     hash_map[nums[left]] -= 1
                     if hash_map[nums[left]] == 0:
-                        dist_count += 1
+                        del hash_map[nums[left]]
                     left += 1
-                total += r - left
+                total += r - left + 1    
             return total
         return atMostK(k) - atMostK(k-1)
