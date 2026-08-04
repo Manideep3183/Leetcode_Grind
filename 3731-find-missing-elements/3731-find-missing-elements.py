0b9b9cbd72734452eb4feb15116b72c8cf7bd78a
P MANIDEEP REDDY <patlollamanideepreddy3183@gmail.com>
2026-08-04 21:53:57 +0530

    Time: 7 ms (52.53%), Space: 12.5 MB (17.28%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.py
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -1,11 +1,14 @@
 class Solution(object):
     def findMissingElements(self, nums):
         res = []
+        vis_num = set(nums)
         min_val = min(nums)
         max_val = max(nums)
-        for num in range(min_val,max_val+1):
-            if num not in nums:
-                res.append(num)
+        next_num = min_val + 1
+        while next_num < max_val:
+            if next_num not in vis_num:
+                res.append(next_num)
+            next_num += 1
         return res
         
         
