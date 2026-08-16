f3a758ff12a82f0144409597c944c34591c4c3b5
P MANIDEEP REDDY <patlollamanideepreddy3183@gmail.com>
2026-08-16 12:12:16 +0530

    Time: 82 ms (7.14%), Space: 20.2 MB (85.71%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def stoneGameIX(self, stones):
+        c0 = c1 = c2 = 0
+        for x in stones:
+            rem = x % 3
+            if rem == 0:
+                c0 += 1
+            elif rem == 1:
+                c1 += 1
+            else:
+                c2 += 1
+
+        if c0 % 2 == 0:
+            return c1 > 0 and c2 > 0
+        else:
+            return abs(c1 - c2) > 2
