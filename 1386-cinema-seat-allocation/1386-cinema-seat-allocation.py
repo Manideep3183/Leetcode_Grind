1c299b9b1b0c09d95964a2f569a70f896cd2f8b4
P MANIDEEP REDDY <patlollamanideepreddy3183@gmail.com>
2026-08-19 19:27:36 +0530

    Time: 23 ms (93.32%), Space: 22.7 MB (70.69%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
@@ -2,18 +2,17 @@ from collections import defaultdict
 
 class Solution:
     def maxNumberOfFamilies(self, n: int, reservedSeats: list[list[int]]) -> int:
-        # Map each row to a bitmask of reserved seats (bits 1 to 10)
+        
         reserved_rows = defaultdict(int)
         for r, c in reservedSeats:
             reserved_rows[r] |= (1 << c)
             
-        # Start by assuming every empty row gets 2 groups
+        
         ans = (n - len(reserved_rows)) * 2
         
-        # Bitmasks for seat blocks
-        LEFT_MASK   = (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5) # 0b0000111100 = 60
-        RIGHT_MASK  = (1 << 6) | (1 << 7) | (1 << 8) | (1 << 9) # 0b1111000000 = 960
-        MIDDLE_MASK = (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7) # 0b0011110000 = 240
+        LEFT_MASK   = (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5)
+        RIGHT_MASK  = (1 << 6) | (1 << 7) | (1 << 8) | (1 << 9)
+        MIDDLE_MASK = (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7) 
         
         for mask in reserved_rows.values():
             left_free = (mask & LEFT_MASK) == 0
