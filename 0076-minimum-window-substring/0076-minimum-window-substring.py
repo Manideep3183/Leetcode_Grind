2a62442d85a0f99fd0f1f77beef5629b5b167802
P MANIDEEP REDDY <patlollamanideepreddy3183@gmail.com>
2026-08-23 14:40:42 +0530

    Time: 130 ms (26.72%), Space: 19.5 MB (94.44%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,25 +1,23 @@
 from collections import Counter
-class Solution():
-    def minWindow(self,s,t):
-        if not t or not s:
-            return ""
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if not s or not t or len(t)>len(s):
+            return ''
+        count = len(t)
         freq = Counter(t)
         min_len = float('inf')
-        left = 0
+        l = 0
         ans = ''
-        count = len(t)
-        for right in range(len(s)):
-            if freq[s[right]] > 0:
+        for r in range(len(s)):
+            if freq[s[r]] > 0:
                 count -= 1
-            freq[s[right]] -= 1
-
+            freq[s[r]] -= 1
             while count == 0:
-                if min_len > right-left+1:
-                    ans = s[left :  right+1]
-                    min_len = right-left+1
-                
-                freq[s[left]] += 1
-                if freq[s[left]] > 0:
+                if min_len > r - l + 1:
+                    ans = s[l:r+1]
+                    min_len = r-l+1
+                freq[s[l]] += 1
+                if freq[s[l]] > 0:
                     count += 1
-                left += 1
+                l += 1
         return ans
