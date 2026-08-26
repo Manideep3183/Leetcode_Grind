3312159ac6438c03d18452c372ea5c6e6cd4b620
P MANIDEEP REDDY <patlollamanideepreddy3183@gmail.com>
2026-08-26 15:35:13 +0530

    Time: 42 ms (66.07%), Space: 23.9 MB (98.48%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
@@ -1,19 +1,16 @@
-class Solution(object):
-    def maxSubarraySumCircular(self, nums):
-        max_sum=nums[0]
-        curr_max=0
-        min_sum=nums[0]
-        curr_min=0
-        total_sum=0
-        #Kadanes for both MIN and MAX sum
-        #MAX SUM = Total - MIN SUM
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        max_sum = nums[0]
+        curr_max = 0
+        min_sum = nums[0]
+        curr_min = 0
+        total = 0
         for num in nums:
-            curr_max=max(num,curr_max+num)
-            max_sum=max(curr_max,max_sum)
-            curr_min=min(num,curr_min+num)
-            min_sum=min(curr_min,min_sum)
-            total_sum+=num
-        if total_sum==min_sum:
-                return max_sum
-        return max(max_sum,total_sum-min_sum)
-        
+            curr_max = max(curr_max + num, num)
+            max_sum = max(curr_max, max_sum)
+            curr_min = min(curr_min + num, num)
+            min_sum = min(curr_min, min_sum)
+            total += num
+        if max_sum < 0 :
+            return max_sum
+        return max(max_sum, total - min_sum)
