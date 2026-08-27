6e539d304b7406d9c6f5d57ee007d8bf04d084ea
P MANIDEEP REDDY <patlollamanideepreddy3183@gmail.com>
2026-08-27 18:44:45 +0530

    Time: 32 ms (88.56%), Space: 29.7 MB (83.26%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,23 +1,54 @@
-from collections import OrderedDict
-class LRUCache(object):
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.val = value
+        self.prev = None
+        self.next = None
 
-    def __init__(self, capacity):
-        self.cache = OrderedDict()
+class LRUCache:
+
+    def __init__(self, capacity: int):
         self.cap = capacity
+        self.cache = {}
+        self.right = Node(0,0)
+        self.left = Node(0,0)
+        self.left.next = self.right
+        self.right.prev = self.left
+    def remove(self, node):
+        next = node.next
+        prev = node.prev
+        prev.next = next
+        next.prev = prev
 
-    def get(self, key):
+    def insert(self, node):
+        prev = self.right.prev
+        next = self.right
+
+        node.next = next
+        node.prev = prev
+
+        prev.next = node
+        next.prev = node
+
+    def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
-        value = self.cache.pop(key) 
-        self.cache[key] = value
-        return self.cache[key]
-    def put(self, key, value):
+        node = self.cache[key]
+        self.remove(node)
+        self.insert(node)
+        return node.val
+
+    def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.cache.pop(key) 
-        elif len(self.cache) >= self.cap:
-            self.cache.popitem(last=False)
-        self.cache[key] = value
-        
+            self.remove(self.cache[key])
+        node = Node(key,value)
+        self.cache[key] = node
+        self.insert(node)
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
 
 
 # Your LRUCache object will be instantiated and called as such:
