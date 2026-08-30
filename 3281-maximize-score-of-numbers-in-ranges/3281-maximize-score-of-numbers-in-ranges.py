988cba7acbc6b15735c37042997f1780d9ce54a8
P MANIDEEP REDDY <patlollamanideepreddy3183@gmail.com>
2026-08-30 20:57:51 +0530

    Time: 1272 ms (34.48%), Space: 35.5 MB (56.9%) - LeetHub

---
+++ 3281-maximize-score-of-numbers-in-ranges/3281-maximize-score-of-numbers-in-ranges.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxPossibleScore(self, start: list[int], d: int) -> int:
+        start.sort()
+        n = len(start)
+
+        def can_achieve(score: int) -> bool:
+            prev = start[0]
+            for i in range(1, n):
+                
+                next_val = max(start[i], prev + score)
+                if next_val > start[i] + d:
+                    return False
+                prev = next_val
+            return True
+
+     
+        low = 0
+        high = start[-1] + d 
+        ans = 0
+
+        while low <= high:
+            mid = (low + high) // 2
+            if can_achieve(mid):
+                ans = mid
+                low = mid + 1 
+            else:
+                high = mid - 1  
+
+        return ans
